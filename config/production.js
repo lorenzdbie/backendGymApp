fa5fb7afc23249ff28f9ac03f27ee614de19bb56
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-22 17:02:23 +0100

    env aanpassing

--- config/production.js
+++ config/production.js
@@ -8,12 +8,12 @@ module.exports = {
     origins: ['https://two223-frontendweb-lorenzdebie.onrender.com'],
     maxAge: 3 * 60 * 60,
   },
-  database: {
-    client: 'mysql2',
-    host: 'vichogent.be',
-    port: 40043,
-    name: '702547ld',
-    username: '702547ld',
-    password: 'nEO2l9MSWZoij3qloyHl',
-  },
+  // database: {
+  //   client: 'mysql2',
+  //   host: 'vichogent.be',
+  //   port: 40043,
+  //   name: '702547ld',
+  //   username: '702547ld',
+  //   password: 'nEO2l9MSWZoij3qloyHl',
+  // },
 };
