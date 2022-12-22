659ae68e1c660b16a25e3701e8c61a351ac20ab7
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-22 17:22:45 +0100

    laatste aanpassing .env

--- config/production.js
+++ config/production.js
@@ -16,20 +16,4 @@ module.exports = {
     username: 'DATABASE_USERNAME',
     password: 'DATABASE_PASSWORD',
   },
-  // database: {
-  //   client: 'mysql2',
-  //   host: config.get('database.host'),
-  //   port: config.get('database.port'),
-  //   name: config.get('database.name'),
-  //   username: config.get('database.username'),
-  //   password: config.get('database.password'),
-  // },
-  // database: {
-  //   client: 'mysql2',
-  //   host: 'vichogent.be',
-  //   port: 40043,
-  //   name: '702547ld',
-  //   username: '702547ld',
-  //   password: 'nEO2l9MSWZoij3qloyHl',
-  // },
 };
